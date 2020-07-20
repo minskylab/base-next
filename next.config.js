9a52b455c2cc912cdcc41bf603b16626479177f9
Bregy Malpartida Ramos <bregy.malpartida@utec.edu.pe>
2020-07-20 17:54:31 -0500

    update webpack loader

--- next.config.js
+++ next.config.js
@@ -1,16 +1,22 @@
 const withCSS = require("@zeit/next-css");
 
 module.exports = withCSS({
-    webpack(config, { isServer }) {
-        config.module.rules[0].use = [
-            config.module.rules[0].use,
-            {
-                loader: "linaria/loader",
-                options: {
-                    sourceMap: process.env.NODE_ENV !== "production"
-                }
-            }
-        ];
+    webpack(config) {
+        // retrieve the rule without knowing its order
+        const jsLoaderRule = config.module.rules.find(
+            (rule) => rule.test instanceof RegExp && rule.test.test('.js')
+        );
+        const linariaLoader = {
+            loader: 'linaria/loader',
+            options: {
+                sourceMap: process.env.NODE_ENV !== 'production',
+            },
+        };
+        if (Array.isArray(jsLoaderRule.use)) {
+            jsLoaderRule.use.push(linariaLoader);
+        } else {
+            jsLoaderRule.use = [jsLoaderRule.use, linariaLoader];
+        }
         return config;
-    }
+    },
 });
