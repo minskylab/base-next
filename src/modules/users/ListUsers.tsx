087ddbb144f45d2b7c153312f6c10b88e610b63c
Bregy Malpartida <bregy@minsky.cc>
2021-07-14 19:31:26 -0500

    feat: added graphql codegen integration and a little example of use

---
+++ src/modules/users/ListUsers.tsx
@@ -0,0 +1,50 @@
+import { Box, List, ListIcon, ListItem, Skeleton, Stack, Text, useToast } from "@chakra-ui/react";
+import { useListUsersQuery } from "integration/graphql";
+import { CheckIcon } from "@chakra-ui/icons";
+import { useEffect } from "react";
+type ListUsersProps = {};
+
+const ListUsers = ({}: ListUsersProps) => {
+  const [{ data, fetching, error }] = useListUsersQuery();
+  const toast = useToast();
+
+  useEffect(() => {
+    error &&
+      toast({
+        title: "GraphQL Query Error",
+        description: error.message,
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+      });
+  }, [error]);
+
+  return (
+    <Box>
+      <Text textAlign={"center"} my={3} color={"gray.500"}>
+        And
+      </Text>
+      <Box>
+        {fetching && (
+          <Stack>
+            <Skeleton height="1.25rem" isLoaded={!fetching} />
+            <Skeleton height="1.25rem" isLoaded={!fetching} />
+            <Skeleton height="1.25rem" isLoaded={!fetching} />
+            <Skeleton height="1.25rem" isLoaded={!fetching} />
+            <Skeleton height="1.25rem" isLoaded={!fetching} />
+          </Stack>
+        )}
+        <List spacing={3}>
+          {data?.users?.data?.map(user => (
+            <ListItem>
+              <ListIcon as={CheckIcon} color="green.500" />
+              <b>Hello</b> {user?.name}
+            </ListItem>
+          ))}
+        </List>
+      </Box>
+    </Box>
+  );
+};
+
+export { ListUsers };
