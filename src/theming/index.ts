ab94407a2ffd1f44aa859472d9cc1acc833c0768
Bregy Malpartida <bregy@minsky.cc>
2021-07-28 20:08:59 -0500

    fix: little fixes for theming feature

--- src/theming/index.ts
+++ src/theming/index.ts
@@ -1,29 +1,30 @@
-import { Colors, extendTheme, ThemeConfig } from "@chakra-ui/react";
+import { Colors, TypographyProps, extendTheme, ThemeConfig } from "@chakra-ui/react";
 import { GlobalStyles } from "@chakra-ui/theme-tools";
 
 const config: ThemeConfig = {
   // initialColorMode: "dark",
-  useSystemColorMode: false,
+  useSystemColorMode: true,
 };
 
 const colors: Colors = {
   primary: "#8F98FE",
   secondary: "#FEE7AF",
-  // backgroundDark: "#002832",
-  // backgroundDarkLight: "#002832",
-  // backgroundDarkLight: "#0a303a",
-  // backgroundDarkDeep: "#00222b",
-  // backgroundDark: "#00222b",
+};
+
+const fonts = {
+  body: "Open Sans, system-ui, sans-serif",
+  heading: "Georgia, serif",
+  mono: "Menlo, monospace",
 };
 
 const styles: GlobalStyles = {
   global: () => ({
     "html, body": {
-      backgroundColor: "backgroundDark",
+      backgroundColor: "dark",
     },
   }),
 };
 
-const customTheme = extendTheme({ colors, config, styles });
+const customTheme = extendTheme({ colors, config, styles, fonts });
 
 export { customTheme };
